a1e0caa4f011ced4b4c063dae52cb421f94194ab
sonofmercy123 <Ojekunlep123@gmail.com>
2022-09-08 12:24:31 -0700

    u

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,13 @@
 /* main - Print the string in the function
  *
  * Description: using the main function 
- * this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",followed by a new line, to standard error.
+ * this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * followed by a new line, to standard error.
  * Return: Always 1 (succes)
  */
 int main(void)
 {       
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);    
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",    
+	        sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));                                                                         
 	return (1);
 }
