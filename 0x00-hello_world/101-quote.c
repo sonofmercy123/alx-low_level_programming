55ab5e6d2e1f2d76c30caac4debef6f936d46b62
sonofmercy123 <Ojekunlep123@gmail.com>
2022-09-08 12:20:12 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /* main - Print the string in the function
  *
  * Description: using the main function 
- * this program prints "programming is like a building multilingual function
+ * this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",followed by a new line, to standard error.
  * Return: Always 1 (succes)
  */
 int main(void)
 {       
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);                                                                                 return (1);
-}	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);    
+	return (1);
+}
