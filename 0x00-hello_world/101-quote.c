1d122d5d147003043da1e3503cbc39ba8dbdef63
sonofmercy123 <Ojekunlep123@gmail.com>
2022-09-08 11:59:53 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>   
 
-/* main - Entry point
+/* main - Print the string in the function
  *
- * Return: Always 0 (succes)
+ * Description: using the main function 
+ * this program prints "programming is like a building multilingual function
+ * Return: Always 1 (succes)
  */
- int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",        61);
-	return (1);
-}
+int main(void)
+{       
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);                                                                                 return (1);
+}	
