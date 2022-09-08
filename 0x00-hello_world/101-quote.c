9a56d53dd97e9ed1a9a15088c07d5ae106d83993
sonofmercy123 <Ojekunlep123@gmail.com>
2022-09-08 08:34:59 -0700

    Intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,11 @@
  * 
  * Discription: using the main function
  * this program prints "programming is like a building multilingual function
- * Return: 1
+ * Return: 0
  */   
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
