be64a9b643f71966f369aa5e6515d29ab7bc9074
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-09 22:28:02 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,45 +1,23 @@
 /*
- *
- *  * File: 2-print_alphabet.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
+ * File: 2-print_alphabet.c
+ * Auth: Brennan D Baraban
+ */
 
 #include <stdio.h>
 
-
-
 /**
+ * main - Prints the alphabet in lowercase.
  *
- *  * main - Prints the alphabet in lowercase.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	char letter;
 
-		char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
+	putchar('\n');
 
-
-			for (letter = 'a'; letter <= 'z'; letter++)
-
-						putchar(letter);
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
