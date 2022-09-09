393397a9f9363d2b04011d3210f249c801f465d3
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-09 23:11:49 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
-#include <stdio.h>                                                              
-
-#include <unistd.h>  
-
-/**
- * main  - print the string in the function 
- *
- * Discription: using the main function
- * this program prints "programming is like a building multilingual function
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-w	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
-}
+#include <stdio.h>                                                                                                                                              
+                                                                                                                                                                
+#include <unistd.h>                                                                                                                                             
+                                                                                                                                                                
+/**                                                                                                                                                             
+ * main - print the string in the function                                                                                                                      
+ *                                                                                                                                                              
+ * Discription: using the main function                                                                                                                         
+ * this program prints "programming is like a building multilingual function                                                                                    
+ * Return: Always 1 (Success)                                                                                                                                   
+ */                                                                                                                                                             
+int main(void)                                                                                                                                                  
+{                                                                                                                                                               
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",                                                                               
+        sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));                                                                                 
+        return (1);                                                                                                                                             
+}                              
