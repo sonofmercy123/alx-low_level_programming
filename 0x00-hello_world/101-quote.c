ab749af0ae29d924d0df2894c216f2227d4a6323
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-09 23:12:40 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
-#include <stdio.h>                                                                                                                                              
-                                                                                                                                                                
-#include <unistd.h>                                                                                                                                             
-                                                                                                                                                                
-/**                                                                                                                                                             
- * main - print the string in the function                                                                                                                      
- *                                                                                                                                                              
- * Discription: using the main function                                                                                                                         
- * this program prints "programming is like a building multilingual function                                                                                    
- * Return: Always 1 (Success)                                                                                                                                   
- */                                                                                                                                                             
-int main(void)                                                                                                                                                  
-{                                                                                                                                                               
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",                                                                               
-        sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));                                                                                 
-        return (1);                                                                                                                                             
-}                              
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
