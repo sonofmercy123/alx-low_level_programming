72e4d68eb1022ff6f12afc9c1265a0886fa4dba8
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-13 21:30:03 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
-#include "main.h"
-
-/**
- * print _alphabet -> prints the lowercase alphabets
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
  */
 
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
-	int j;
-	
-	for (j = 'a'; j <= 'z'; j++)
-	{
-		_putchar(j);
-	}
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
-	}
+}
