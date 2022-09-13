979282ed2d32a2d7e27807cab263cbfba6a2cbd9
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-13 19:50:39 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main"
 
-/**
- * main - print putchar
- * Return: always 0
- */
-
+/** main - prints putchar
+* Return: always 0
+*/
 int main(void)
 {
 	_putchar('_');
@@ -18,3 +16,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+}
