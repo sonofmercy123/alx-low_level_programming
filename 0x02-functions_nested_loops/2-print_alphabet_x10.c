f623d176492d66963515b0df5c473b4c72ce1863
sonofmercy123 <Ojekunlep123@gmail.com>
2022-09-13 13:38:55 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+i#include "main.h"
+
+
+
+/**
+ *
+ *  * print _alphabet -> prints the lowercase alphabets
+ *
+ *   */
+
+
+
+void print_alphabet(void)
+
+{
+
+		int j;
+
+			
+
+			for (j = 'a'; j <= 'z'; j++)
+
+					{
+
+								_putchar(j);
+
+									}
+
+				_putchar('\n');
+
+					}`
