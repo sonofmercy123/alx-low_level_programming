7b1a7ea9dd4f142af05766891c11fad56e03fae9
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-14 11:29:04 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,13 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n);
+Int print_sign(int n);
+Int _abs(int);
+Int print_last_digit(int);
+Void jack_bauer(void);
+Void times_table(void);
+Int add(int a, int b);
+Void print_to_98(int n);
+Void print_times_table(int n);
+Void num(int r, int n);
+#endif
