96857c93aa4a56cc6dcc41c35a47c07a27a311f7
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-15 20:19:28 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,16 @@
+0-isupper.c
 #include "main.h"
 #include <ctype.h>
 
 /**
 * _isupper - Checks for uppercase ASCII characters
-* @c: This is an ASCII characte
+* @c: This is an ASCII character
+*
 * Return: 0 if not uppercase, 1 if uppercase
 */
 int _isupper(int c)
 {
-if (isupper(c))
-return (1);
-return (0);
+	if (isupper(c))
+		return (1);
+	return (0);
 }
