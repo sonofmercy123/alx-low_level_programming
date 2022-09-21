73f6eadc9ba06b142231303d420f1ef8eb553470
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-21 19:50:38 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,28 +1,26 @@
-#include "main.h"
-/**
-* cap_string - capitalizes all words of a string
-* @s: input string.
-* Return: the pointer to dest.
-*/
-char *cap_string(char *s)
-{
-int count = 0, i;
-int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-if (*(s + count) >= 97 && *(s + count) <= 122)
-*(s + count) = *(s + count) - 32;
-count++;
-while (*(s + count) != '\0')
-{
-for (i = 0; i < 13; i++)
-{
-if (*(s + count) == sep_words[i])
-{
-if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-*(s + (count + 1)) = *(s + (count + 1)) - 32;
-break;
-}
-}
-count++;
-}
-return (s);
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * _strncpy - Copies at most an inputted number                                                                                      
+ *            of bytes from string src into dest.                                                                                    
+ * @dest: The buffer storing the string copy.                                                                                        
+ * @src: The source string.                                                                                                          
+ * @n: The maximum number of bytes to copied from src.                                                                               
+ *                                                                                                                                   
+ * Return: A pointer to the resulting string dest.                                                                                   
+ */                                                                                                                                  
+char *_strncpy(char *dest, char *src, int n)                                                                                         
+{                                                                                                                                    
+        int index = 0, src_len = 0;                                                                                                  
+                                                                                                                                     
+        while (src[index++])                                                                                                         
+                src_len++;                                                                                                           
+                                                                                                                                     
+        for (index = 0; src[index] && index < n; index++)                                                                            
+                dest[index] = src[index];                                                                                            
+                                                                                                                                     
+        for (index = src_len; index < n; index++)                                                                                    
+                dest[index] = '\0';                                                                                                  
+                                                                                                                                     
+        return (dest);                                                                                                               
 }
