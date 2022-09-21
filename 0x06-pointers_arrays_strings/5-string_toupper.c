99e98c91aac18416671e36c6384ab45ef2b3b11c
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-21 19:54:18 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,17 +10,17 @@
  * Return: A pointer to the resulting string dest.                                                                                   
  */                                                                                                                                  
 char *_strncpy(char *dest, char *src, int n)                                                                                         
-{                                                                                                                                    
-        int index = 0, src_len = 0;                                                                                                  
+{
+int index = 0, src_len = 0;                                                                                                  
                                                                                                                                      
-        while (src[index++])                                                                                                         
-                src_len++;                                                                                                           
+while (src[index++])                                                                                                         
+src_len++;
+  
+for (index = 0; src[index] && index < n; index++)                                                                            
+dest[index] = src[index];                                                                                            
                                                                                                                                      
-        for (index = 0; src[index] && index < n; index++)                                                                            
-                dest[index] = src[index];                                                                                            
+for (index = src_len; index < n; index++)                                                                                    
+dest[index] = '\0';                                                                                                  
                                                                                                                                      
-        for (index = src_len; index < n; index++)                                                                                    
-                dest[index] = '\0';                                                                                                  
-                                                                                                                                     
-        return (dest);                                                                                                               
+return (dest);                                                                                                               
 }
