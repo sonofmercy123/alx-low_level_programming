1c3dcbcffa192db404b8c3186e13c3a0a35304de
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-23 22:01:26 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,28 +1,21 @@
 #include "main.h"
+
 /**
-* cap_string - capitalizes all words of a string
-* @s: input string.
-* Return: the pointer to dest.
-*/
-char *cap_string(char *s)
+ * string_toupper - Changes any lowercase letter in a string to uppercase
+ * @n: string to check
+ * Return: Uppercased string
+ */
+
+char *string_toupper(char *n)
 {
-int count = 0, i;
-int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-if (*(s + count) >= 97 && *(s + count) <= 122)
-*(s + count) = *(s + count) - 32;
-count++;
-while (*(s + count) != '\0')
-{
-for (i = 0; i < 13; i++)
-{
-if (*(s + count) == sep_words[i])
-{
-if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-*(s + (count + 1)) = *(s + (count + 1)) - 32;
-break;
-}
-}
-count++;
-}
-return (s);
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 97 && n[i] <= 122)
+			n[i] = n[i] - 32;
+		else
+			continue;
+	}
+	return (n);
 }
