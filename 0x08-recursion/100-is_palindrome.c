ad2158662971c43a566d2e6719160ea1517f860e
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-09-28 19:54:15 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,13 +12,11 @@ int is_palindrome(char *s);
  */                                                                                                                                   
 int find_strlen(char *s)                                                                                                              
 {                                                                                                                                     
-        int len = 0;                                                                                                                  
-                                                                                                                                      
-        if (*(s + len))                                                                                                               
-        {                                                                                                                             
-                len++;                                                                                                                
-                len += find_strlen(s + len);                                                                                          
-        }                                                                                                                             
-                                                                                                                                      
+int len = 0;                                                                                                                  
+if (*(s + len))                                                                                                              
+{                                                                                                                             
+len++;                                                                                                                
+len += find_strlen(s + len);                                                                                          
+ }                                                                                                                                                                                                                                                                 
         return (len);                                                                                                                 
 }                                                                                                   
