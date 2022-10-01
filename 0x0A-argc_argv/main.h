9dfb5fbe983f176983f00be58c52dd15f4d29a68
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-10-01 13:43:25 +0100

    Update main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,2 @@
-#ifndef main.h
-#define main.h
-#include <stdio.h>
-
-int _putchar(char c);/*prototype _putchar*/
-
-#endif /* MAIN_H */
+int _putchar(char c);                                                                                                                
+int check_num(char *str);
