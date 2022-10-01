18bf795a0184967021ad8f196ca45466f51d9eb5
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-10-01 13:45:19 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-
-/**
- * main -  print name
- *@argc: int.
- *@argv: char.
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-	int i;
-
-	for (i = 0; i < argc ; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	return (0);
+#include <stdio.h>                                                                                                                   
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * main - Print the name of the program                                                                                              
+ * @argc: Count arguments                                                                                                            
+ * @argv: Arguments                                                                                                                  
+ *                                                                                                                                   
+ * Return: Always 0 (Success)                                                                                                        
+ */                                                                                                                                  
+                                                                                                                                     
+int main(int argc, char *argv[])                                                                                                     
+{                                                                                                                                    
+(void) argc;                                                                                                                         
+printf("%s\n", argv[0]);                                                                                                             
+                                                                                                                                     
+return (0);                                                                                                                          
 }
