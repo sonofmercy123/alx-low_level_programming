f9aa4db54bf434f9bd1e3a633a754749d25b11b7
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-10-06 13:14:22 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptrtomem;
 
-#endif /*MAIN_H*/
+	ptrtomem = malloc(b);
+	if (ptrtomem == NULL)
+		exit(98);
+	return (ptrtomem);
+}
