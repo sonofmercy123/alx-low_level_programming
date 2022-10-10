642b9be944037e3fdbdedeb0229917efe0e47d3f
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-10-10 12:41:17 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * File: dog.h
- * Auth: Brennan D Baraban
- * Desc: Header file that defines a new type struct dog.
- */
-
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - A simple struct that contains variables
+ * @name: Holds name of dog in char array
+ * @age: Holds number, including any decimals
+ * @owner: Holds name of owner in char array
  */
 struct dog
 {
@@ -19,12 +13,12 @@ struct dog
 	float age;
 	char *owner;
 };
-
 /**
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
