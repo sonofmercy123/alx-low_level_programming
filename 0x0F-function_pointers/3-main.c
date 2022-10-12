b07cc330933662998de0cb54af8d7b9b3b3607ce
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-10-12 18:05:27 +0100

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - Adds two integers
+ * @a: first addend
+ * @b: second addend
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Subtracts two integers
+ * @a: first number to subtract
+ * @b: second number to subtract
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Multiplies two integers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divide two integers
+ * @a: first number to divide
+ * @b: second number to divide
+ *
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - finds the remainder of the division of two ints
+ * @a: first number
+ * @b: second second number
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
