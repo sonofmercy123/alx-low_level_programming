6cb8d3e2e6c5358f9891a13cb85e1b8c4e2f10ec
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-10-13 21:16:51 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,85 +1,22 @@
 #include "variadic_functions.h"
-
 #include <stdarg.h>
 
-
-
 /**
- *
- *  * sum_them_all - returns the sum of all its parameters
- *
- *   * @n: number of arguments
- *
- *    * Return: returns the sum of all its parameters
- *
- *     */
-
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of arguments
+ * Return: returns the sum of all its parameters
+ */
 int sum_them_all(const unsigned int n, ...)
-
 {
+	va_list ap;
+	unsigned int i;
+	int sum = 0;
 
-		va_list ap;
-
-			unsigned int i;
-
-				int sum = 0;
-
-
-
-					if (n == 0)
-
-								return (0);
-
-						va_start(ap, n);
-
-							for (i = 0; i < n; i++)
-
-										sum += va_arg(ap, int);
-
-								va_end(ap);
-
-									return (sum);
-
-}i#include "variadic_functions.h"
-
-#include <stdarg.h>
-
-
-
-/**
- *
- *  * sum_them_all - returns the sum of all its parameters
- *
- *   * @n: number of arguments
- *
- *    * Return: returns the sum of all its parameters
- *
- *     */
-
-int sum_them_all(const unsigned int n, ...)
-
-{
-
-		va_list ap;
-
-			unsigned int i;
-
-				int sum = 0;
-
-
-
-					if (n == 0)
-
-								return (0);
-
-						va_start(ap, n);
-
-							for (i = 0; i < n; i++)
-
-										sum += va_arg(ap, int);
-
-								va_end(ap);
-
-									return (sum);
-
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
 }
