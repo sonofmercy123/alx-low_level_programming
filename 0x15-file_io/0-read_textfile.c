d10095c88ac6cba4d22c4fbdbc58f85e10f689d6
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2022-10-31 22:07:58 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,89 +1,45 @@
-nclude "holberton.h"
-
+#include "main.h"
 /**
+ * read_textfile - Reads a text file and prints iti to the POSIX stdout
+ * @filename: text file to read
+ * @letters: number of letters it should read and print
  *
- *  * read_textfile - Reads a text file and prints iti to the POSIX stdout
- *
- *   * @filename: text file to read
- *
- *    * @letters: number of letters it should read and print
- *
- *     *
- *
- *      * Return: number of letters it could read and print
- *
- *       */
-
+ * Return: number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
-
-		int fd;
-
-			char *buffer;
-
-				ssize_t readcount = 0;
-
-					ssize_t writecount = 0;
+	int fd;
+	char *buffer;
+	ssize_t readcount = 0;
+	ssize_t writecount = 0;
 
 
-
-
-
-						if (filename == NULL)
-
-									return (0);
-
-							buffer = malloc(letters);
-
-								if (buffer == NULL)
-
-											return (0);
-
-									fd = open(filename, O_RDONLY);
-
-										if (fd == -1)
-
-												{
-
-															free(buffer);
-
-																	return (0);
-
-																		}
-
-											readcount = read(fd, buffer, letters);
-
-												if (readcount == -1)
-
-														{
-
-																	free(buffer);
-
-																			close(fd);
-
-																					return (0);
-
-																						}
-
-													writecount = write(STDOUT_FILENO, buffer, readcount);
-
-														if (writecount == -1)
-
-																{
-
-																			free(buffer);
-
-																					close(fd);
-
-																							return (0);
-
-																								}
-
-															close(fd);
-
-																free(buffer);
-
-																	return (writecount);
-
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	readcount = read(fd, buffer, letters);
+	if (readcount == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	writecount = write(STDOUT_FILENO, buffer, readcount);
+	if (writecount == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (writecount);
 }
